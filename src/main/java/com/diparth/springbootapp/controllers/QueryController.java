9c7153a198ed79b850b0483bc19d3dbdb79982fd
diparthpatel <dpate168@nyit.edu>
2021-04-24 15:35:32 +0530

    minor changes

--- src/main/java/com/diparth/springbootapp/controllers/QueryController.java
+++ src/main/java/com/diparth/springbootapp/controllers/QueryController.java
@@ -1,23 +1,25 @@
 package com.diparth.springbootapp.controllers;
 
-import com.diparth.springbootapp.entities.QueryReport;
-import com.diparth.springbootapp.repositories.QueryRepository;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.diparth.springbootapp.entities.QueryReport;
+import com.diparth.springbootapp.repositories.QueryRepository;
+
 @Controller
 public class QueryController {
+
+    @Autowired
     QueryRepository queryRepository;
 
-    @RequestMapping("/")
-    public String index() {
-        return "welcome";
+    public QueryController(QueryRepository queryRepository) {
+        System.out.println("Inside constructor -----> " + queryRepository.toString());
+        this.queryRepository = queryRepository;
     }
 
     @GetMapping("/query")
@@ -30,7 +32,7 @@ public class QueryController {
 
     @PostMapping("/query")
     public String postQuery(QueryReport queryReport, RedirectAttributes ra) {
-        this.queryRepository.save(queryReport);
+        queryRepository.save(queryReport);
 
         ra.addAttribute("submitted", true);
         
